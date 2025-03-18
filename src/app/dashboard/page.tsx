0290672b8cb1e44a8011292ec3c3564c58f02574
ervin-diaz <Geineryodan@gmail.com>
2025-03-18 14:19:36 -0600

    Create product form and inser data into the  database

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,3 +1,11 @@
-export default function DashboardPage() {
+import { getProducts } from '@/server/db/products';
+import { auth } from '@clerk/nextjs/server'
+import { NoProducts } from './_componets/NoProducts';
+
+export default async function DashboardPage() {
+    const { userId, redirectToSignIn } = await auth();
+    if (userId == null) return redirectToSignIn();
+    const products = await getProducts(userId, { limit: 6 });
+    if (products.length === 0) return <NoProducts />
     return null;
 }
